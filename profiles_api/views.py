0e440cd07e01b591eb104e698ca4f8bc13168adf
mahmud92542 <mhasan92542@gmail.com>
2021-03-01 16:58:11 +0600

    add custom profile authentication

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .models import *
 from .serializers import *
+from .permissions import *
+
 
 from rest_framework import status, viewsets
-
+from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
 class UserProfileViewSet(viewsets.ModelViewSet):
 	"Handle creating & updating profiles"
 	queryset = UserProfile.objects.all()
 	serializer_class = UserProfileSerializer
+	"authentication"
+	authentication_classes = (TokenAuthentication,)
+	permission_classes = (UpdateOwnProfile,)
